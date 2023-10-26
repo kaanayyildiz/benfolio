9d153e3bdbeb335c21e8e0314936021b42ca8bd2
kaanayyildiz <ykaanayyildiz@gmail.com>
2023-10-26 16:11:13 +0300

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <div className="px-2 py-2">
         <div className="flex items-center justify-center text-center px-7 py-7 bg-gray-100 rounded-lg">
           <p className="w-full">
-          © 2023 Benfolio – React Template by <a href="github.com/kaanayyildiz" className="font-medium underline" >Kaan</a>
+          © 2023 Benfolio – React Template by <a href="https://github.com/kaanayyildiz" className="font-medium underline" >Kaan</a>
           </p>
         </div>
       </div>
